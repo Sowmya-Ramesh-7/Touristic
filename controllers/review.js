88fa262d49cab0d713b04c281f8ad91c12f4ef60
Sowmya Ramesh <soumyaramesh2002@gmail.com>
2023-11-26 12:01:38 +0530

    Averge Rating to know Trending listing

--- controllers/review.js
+++ controllers/review.js
@@ -3,10 +3,18 @@ const Review=require("../models/review.js");
 const Listing=require("../models/listing.js");
 
 module.exports.createReviews=async(req,res)=>{
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review)
-    newReview.author=req.user._id
+    let listing=await Listing.findById(req.params.id).populate("reviews");
+    console.log(listing)
+    let newReview=new Review(req.body.review);
+    newReview.author=req.user._id;
     listing["reviews"].push(newReview);
+    console.log(listing["reviews"].length)
+    let total=0;
+    for(let review of listing["reviews"]){
+        console.log(review)
+        total+= review.rating
+    }
+    listing["avgRating"]=total/listing["reviews"].length
     await newReview.save();
     await listing.save();
     req.flash("success","New Review Created");
@@ -15,8 +23,10 @@ module.exports.createReviews=async(req,res)=>{
 
 module.exports.destroyReviews=async(req,res)=>{
     let {id,reviewId}=req.params;
-    await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
-    await Review.findByIdAndDelete(reviewId);
+    let listing=await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
+    let deletedReview=await Review.findByIdAndDelete(reviewId);
+    listing["avgRating"]=((listing["avgRating"]*(listing["reviews"].length+1))-deletedReview.rating)/listing["reviews"].length;
+    listing.save()
     req.flash("success","Review Deleted");
     res.redirect(`/listings/${id}`)
 }
