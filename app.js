c63709378cbab46018fd162a325a530557d24030
Sowmya Ramesh <soumyaramesh2002@gmail.com>
2023-11-26 15:09:35 +0530

    mongo atlas

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+if(process.env.NODE_ENV!="production"){
+    require("dotenv").config();
+}
+
+
 const fs = require('fs');
 const express=require("express");
 const app=express();
@@ -20,6 +25,7 @@ const favoriteRouter=require("./routes/favorite.js");
 
 const multer  = require('multer');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const passport = require('passport');
 const LocalStrategy=require('passport-local');
 const User=require("./models/user.js")
@@ -33,6 +39,8 @@ app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
 
+const dbUrl=process.env.ATLASDB_URL;
+
 
 main()
     .then(()=>{
@@ -43,12 +51,25 @@ main()
     });
 
 async function main(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/touristic");
+    await mongoose.connect(dbUrl);
 }
 
-//sessions
+//sessions, session store
+const store=MongoStore.create({
+    mongoUrl:dbUrl,
+    crypto:{
+        secret: process.env.SECRET,
+    },
+    touchAfter:24*3600,
+})
+
+store.on("error",()=>{
+    console.log("Error on Mongo Session Store",err);
+})
+
 const sessionOptions={
-    secret:"mysupersecret",
+    store:store,
+    secret:process.env.SECRET,
     resave:false,
     saveUninitialized:true,
     cookie:{
@@ -57,6 +78,8 @@ const sessionOptions={
         httpOnly:true
     }
 }
+
+
 app.use(session(sessionOptions));
 app.use(flash())
 
